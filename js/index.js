e5d98c21cbaa8f6a39a9408cb8a0e62ca330c745
slqpez <santiago.lopez16@udea.edu.co>
2020-08-01 19:38:46 -0500

    Finished.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const shareBtn = document.querySelector("#share-btn");
-
+const info = document.querySelector(".info-person");
 shareBtn.addEventListener("click", showSocials);
 
 let open = false;
@@ -8,10 +8,18 @@ function showSocials(e) {
   if (!open) {
     shareBtn.children[0].style.display = "flex";
     shareBtn.children[0].style.cursor = "default";
+    shareBtn.visibility = "hidden";
+    info.style.display = "none";
     open = true;
   } else {
-    open = false;
-    shareBtn.children[0].style.display = "none";
+    if (
+      e.target.classList.contains("share-btn") ||
+      e.target.classList.contains("share-movil")
+    ) {
+      open = false;
+      shareBtn.children[0].style.display = "none";
+      info.style.display = "flex";
+    }
   }
 
   e.preventDefault();
